00c1348d7b880619627772b7e098617b1002732a
sans0987 <fairyfaye0226@gmail.com>
2018-07-26 20:39:18 +0000

    automatically backed up by learn

--- music.rb
+++ music.rb
@@ -5,8 +5,9 @@ list_artists{
   :Lin manuel meranda => "my shot"
   :Twenty one pilots => "HeavyDirtySoul"
   :Moulin rouge => "El Tango De Roxanne"
-  :Coco => "La Llorona"
-}
+  :Coco => "La Llorona"}
+end 
+
 list_artists.key
 
 #2. Define a method named `list_songs`. This method should use the `each_value` method to iterate over the music library hash and print out the name of the songs in the library.
